23ae33319e806671d3cca10aee027f229eb4dd05
V-Mary <74894916+V-Mary@users.noreply.github.com>
2021-04-09 22:16:08 +0300

    Delete lab3/models directory

--- lab3/models/technican.py
+++
@@ -1,7 +0,0 @@
-from models.maker import Maker
-
-
-class Technical(Maker):
-    def __init__(self, name, job, salery, working_hours, film_type, age, number_of_fimls, films_rating, equipment: str):
-        super().__init__(name, job, salery, working_hours, film_type, age, number_of_fimls, films_rating)
-        self.equipment = equipment
